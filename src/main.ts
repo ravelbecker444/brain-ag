d8fe18ad0f87654830ad3d753848e745c6bfae93
Ravel Becker <ravel.beckermhd@gmail.com>
2025-06-03 21:56:32 -0300

    vercel

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: ['http://localhost:3001', 'https://brain-ag-frontend.vercel.app'],
     credentials: true,
   });
 
